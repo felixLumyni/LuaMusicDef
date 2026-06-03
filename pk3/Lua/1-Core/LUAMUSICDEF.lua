3e30986837a0bf4b86dc44ac33793d743c6a168a
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2026-06-02 20:30:46 -0400

    Implement lmd_localadd to add client-side musicdef

--- pk3/Lua/1-Core/LUAMUSICDEF.lua
+++ pk3/Lua/1-Core/LUAMUSICDEF.lua
@@ -12,5 +12,30 @@ LUAMUSICDEF.add = function(key, value)
     assert(type(key) == "string", "Key must be a string.")
     assert(type(value) == "table", "Value must be a table.")
     assert(value.Title, "Value must contain a Title field.")
-    LUAMUSICDEF[key] = value
+    LUAMUSICDEF[key:upper()] = value
 end
+
+--Example: lmd_localadd GFZ2 "cool song" "cool author" "cool game"
+--Only key and title are mandatory
+LUAMUSICDEF.localadd = function(player, key, title, authors, source, bpm, event)
+    local tbl = {}
+    
+    assert(type(key) == "string", "Key must be a string.")
+    assert(type(title) == "string", "Title must be a string.")
+    if authors == "" then authors = nil end
+    if source == "" then source = nil end
+    if bpm == "" then bpm = nil end
+    if event == "" then event = nil end
+
+    tbl.Title = title
+    tbl.Authors = authors
+    tbl.Source = source
+    if type(bpm) == "string" then
+        tbl.BPM = tonumber(bpm) or 0
+    end
+    tbl.Event = event
+
+    LUAMUSICDEF.add(key, tbl)
+end
+
+COM_AddCommand("lmd_localadd", LUAMUSICDEF.localadd, COM_LOCAL)
